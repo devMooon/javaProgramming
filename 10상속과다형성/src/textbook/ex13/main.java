cf5c27fe6158074d247193864b2d839ae436daf8
Seo-Yeon Mun <anstjdus0702@duksung.ac.kr>
2021-11-15 22:47:14 +0900

    인터페이스

---
+++ 10상속과다형성/src/textbook/ex13/main.java
@@ -0,0 +1,12 @@
+package textbook.ex13;
+
+public class main {
+
+	public static void main(String[] args) {
+		Shape donut = new Circle(10);
+		donut.redraw();
+		System.out.println("¸éÀûÀº " + donut.getArea());
+
+	}
+
+}
