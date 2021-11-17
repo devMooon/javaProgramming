9531d946b7b687d303866d566ca1e65c4560e2d2
Seo-Yeon Mun <anstjdus0702@duksung.ac.kr>
2021-11-17 18:52:52 +0900

    ArithmeticException 연습

---
+++ 12예외처리/src/ExceptionEx2_withEH2.java
@@ -0,0 +1,24 @@
+
+public class ExceptionEx2_withEH2 {
+
+	public static int divide(int n1, int n2) throws ArithmeticException {
+		int result = n1 / n2;
+		return result;
+	}
+	public static void main(String[] args) {
+		
+		int n1 = 10;
+		int n2 = 2;
+		
+		try {
+			int result = divide(n1, n2);
+			System.out.println(n1 + "/" + n2 + "=" + result);
+		}
+		catch(ArithmeticException e) {
+			System.err.println(e.getMessage());
+		}
+		System.out.println("³¡\n");
+		
+	}
+
+}
